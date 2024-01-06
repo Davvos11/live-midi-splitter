33ff994455301f76304241c71c7e0b6c759fead0
David Vos <vosdavid2@gmail.com>
2024-01-06 01:49:48 +0100

    add mapping remove button

--- src/gui/tabs/preset.rs
+++ src/gui/tabs/preset.rs
@@ -15,23 +15,30 @@ pub fn preset_tab(ui: &mut Ui, properties: Arc<Mutex<Properties>>, id: usize) {
         inputs.iter().enumerate().for_each(|(input_id, input)| {
             ui.label(input);
             let mapping = preset.mapping.entry(input_id).or_default();
+            let mut maps_to_remove = Vec::new();
 
             mapping.iter_mut().enumerate().for_each(|(map_id, output)| {
-                egui::ComboBox::from_id_source(format!("mapping-{input_id}-{map_id}"))
-                    .selected_text(output.clone())
-                    .width(200.0)
-                    .wrap(true)
-                    .show_ui(ui, |ui| {
-                        ui.style_mut().wrap = Some(true);
-                        available_outputs.iter().for_each(|output_option| {
-                            ui.selectable_value(output, output_option.clone(), output_option);
+                ui.horizontal(|ui| {
+                    if ui.button("X").clicked() {
+                        maps_to_remove.push(map_id);
+                    }
+                    egui::ComboBox::from_id_source(format!("mapping-{input_id}-{map_id}"))
+                        .selected_text(output.clone())
+                        .width(0.5) // FIXME width not working
+                        .wrap(true)
+                        .show_ui(ui, |ui| {
+                            ui.style_mut().wrap = Some(true);
+                            available_outputs.iter().for_each(|output_option| {
+                                ui.selectable_value(output, output_option.clone(), output_option);
+                            });
                         });
-                    });
+                });
             });
 
             if ui.button("Add output").clicked() {
                 mapping.push(String::new());
             }
+            maps_to_remove.iter().for_each(|&i| { mapping.remove(i); });
         });
 
     } else {
