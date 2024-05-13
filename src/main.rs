02dd758633da2e56d6f48d1d9b8e1820ae2897c6
Davvos11 <vosdavid2@gmail.com>
2024-05-13 18:24:47 +0200

    add tracing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 use std::env;
+
+use tracing_subscriber::EnvFilter;
+use tracing_subscriber::fmt::format::FmtSpan;
+
 use crate::gui::Gui;
 
 mod gui;
@@ -18,10 +22,24 @@ fn main() {
         viewport: egui::ViewportBuilder::default().with_inner_size([640.0, 400.0]),
         ..Default::default()
     };
+
+    let filter = EnvFilter::try_from_default_env()
+        .or_else(|_| EnvFilter::try_new("debug"))
+        .unwrap();
+
+    tracing_subscriber::fmt()
+        .with_env_filter(filter)
+        .with_span_events(FmtSpan::CLOSE)
+        .init();
+
     eframe::run_native(
         "Live Midi Splitter",
         options,
-        Box::new(|_cc| {
+        Box::new(|cc| {
+            let mut fonts = egui::FontDefinitions::default();
+            egui_phosphor::add_to_fonts(&mut fonts, egui_phosphor::Variant::Regular);
+
+            cc.egui_ctx.set_fonts(fonts);
             Box::new(gui)
         }),
     ).unwrap();
